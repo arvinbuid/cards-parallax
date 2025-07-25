fb6962617fdd257266ccc41e7f35b551303eed01
Arvin Buid <arvinbuid01@gmail.com>
2025-07-26 01:53:24 +0800

    feat(animation): add smooth scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@
 
 import styles from "./page.module.css";
 import Card from "@/components/Card";
+import Lenis from "lenis";
 
 import { projects } from "./data";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { useScroll } from "framer-motion";
 
 export default function Home() {
@@ -13,6 +14,17 @@ export default function Home() {
     target: containerRef,
     offset: ['start start', 'end end'] // -> top of ref intersects -> top of window (0) -> end of ref intersects -> end of window (1)
   })
+
+  // lenis smooth scroll 
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: number) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+    return () => lenis.destroy(); // cleanup to prevent memory leaks
+  }, [])
 
   return (
     <main ref={containerRef} className={styles.main}>
