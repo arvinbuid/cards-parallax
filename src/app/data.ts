670a26acf3de62c19d85d7bef3f75dfb6b58880d
Arvin Buid <arvinbuid01@gmail.com>
2025-07-25 13:52:00 +0800

    feat(content): add project data and associated images

---
+++ src/app/data.ts
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    title: "Dennis Eichmann",
+    description:
+      "Eichmann, who studied Fine Arts and Fashion Design in Berlin, Kiel, and Vienna, is now based in Berlin, working as a freelance photographer and artist.",
+    src: "seaview.jpg",
+    link: "https://www.ignant.com/2025/02/14/sea-sun-and-soil-dennis-eichmanns-crete-in-analogue/",
+    color: "#BBACAF",
+  },
+  {
+    title: "Saut Hermès",
+    description:
+      "Hermès began not as a fashion house, but as a maker of harnesses, its leatherwork offering a new elegance to the horses whose hooves clipped down the cobbled streets of Paris. From 1837, the then-fledgling atelier outfitted the city’s carriages with bridles and saddles, refining a craft that demanded both function and beauty.",
+    src: "horse-riding.jpg",
+    link: "https://www.ignant.com/2025/04/01/the-saut-hermes-2025-a-spectacle-of-airborne-precision-at-pariss-grand-palais/",
+    color: "#977F6D",
+  },
+  {
+    title: "Leiko Ikemura",
+    description:
+      "Leiko Ikemura creates worlds where the visible and the invisible intertwine. Oscillating between figuration and abstraction, she shapes environments inhabited by mythical beings that blend human and animal features.",
+    src: "water.jpg",
+    link: "https://www.ignant.com/2025/02/05/a-space-that-breathes-inside-the-atelier-of-leiko-ikemura/",
+    color: "#C2491D",
+  },
+];
